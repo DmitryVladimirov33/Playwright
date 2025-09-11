069b07eebeb65e73f8a41e032671f49c8bd82f4d
d.vladimirov <d.vladimirov@ritm.media>
2025-09-12 00:13:18 +0300

    commit_16

--- test_google.py
+++ test_google.py
@@ -1,29 +1,31 @@
 from playwright.sync_api import Playwright
 import re
+import pytest
 
 
-# def test_expert_ui(playwright: Playwright) -> None:
-#     browser = playwright.chromium.launch(headless=False)
-#     context = browser.new_context()
-#     page = context.new_page()
-#     page.goto("https://msk.mrtexpert.ru/about")
-#     page.wait_for_load_state("networkidle")
-#     with page.expect_popup() as page1_info:
-#         page.locator("a.button-sm.button-border-gray-650[href='https://www.telemedex.ru']").click()
-#     page1 = page1_info.value
-#     page1.wait_for_url('https://www.telemedex.ru/')
-#     page1.get_by_role("link", name="Продолжить").click()
-#     page1.goto("https://app.telemedex.ru/telemedex/patient/login")
-#     page1.set_default_navigation_timeout(60)
-#     page1.get_by_role("button", name="Зарегистрироваться").click()
-#     page1.get_by_role("button", name="Вход в личный кабинет").click()
-#
-#     context.close()
-#     browser.close()
-#
+@pytest.mark.regression
+def test_expert_ui(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://msk.mrtexpert.ru/about")
+    page.wait_for_load_state("networkidle")
+    with page.expect_popup() as page1_info:
+        page.locator("a.button-sm.button-border-gray-650[href='https://www.telemedex.ru']").click()
+    page1 = page1_info.value
+    page1.wait_for_url('https://www.telemedex.ru/')
+    page1.get_by_role("link", name="Продолжить").click()
+    page1.goto("https://app.telemedex.ru/telemedex/patient/login")
+    page1.set_default_navigation_timeout(60)
+    page1.get_by_role("button", name="Зарегистрироваться").click()
+    page1.get_by_role("button", name="Вход в личный кабинет").click()
+
+    context.close()
+    browser.close()
+
 
 def test_mrt(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=True)
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://msk.mrtexpert.ru/about")
